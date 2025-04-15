0f290d17845c2e21ad983c55d3a11e3f693bb9c8
ouestcharly <c.bf@live.fr>
2025-04-15 23:49:33 +0200

    Add files via upload

---
+++ data/couchereprage_10.js
@@ -0,0 +1,8 @@
+var json_couchereprage_10 = {
+"type": "FeatureCollection",
+"name": "couchereprage_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Photo": 1.0, "Lien ": null, "Kilomètre": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 6.241888123384734, 46.304268029712468 ] ] } }
+]
+}
